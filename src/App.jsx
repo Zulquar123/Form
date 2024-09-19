5596e5291b424112db25964b5b6d3310d75e3199
code-with-zulquar <codewithzulquar@gmail.com>
2024-09-19 06:02:34 +0530

    Use Ref Updated

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import UseRef from "./UseRef";
+
+export default function App() {
+  return (
+    <div>
+      <UseRef />
+    </div>
+  );
+}
---
+++ src/UseRef.jsx
@@ -0,0 +1,22 @@
+import React, { useRef } from "react";
+
+export default function UseRef() {
+  const sty = "border-2 border-blue-500 rounded m-1 px-1";
+  const name = useRef(null);
+  const age = useRef(null);
+  const email = useRef(null);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name.current.value, age.current.value, email.current.value);
+  };
+  return (
+    <div className="m-10 ">
+      <form action="" onSubmit={handleSubmit}>
+        <input ref={name} type="text" placeholder="name" className={sty} />
+        <input ref={age} type="text" placeholder="age" className={sty} />
+        <input ref={email} type="text" placeholder="email" className={sty} />
+        <input type="submit" className={sty} />
+      </form>
+    </div>
+  );
+}
